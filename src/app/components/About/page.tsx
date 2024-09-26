acb8cc973966eda690db2e58ee0770e99e49aaa3
Muntaha-Noor <muntahanoor648@gmail.com>
2024-09-26 19:16:14 +0500

    Initial commit

---
+++ src/app/components/About/page.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return (
+        <div>
+            <h1 className="text-pink-700 font-bold">This is about page</h1>
+        </div>
+    )
+}
---
+++ src/app/components/Contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact(){
+    return (
+        <div>
+            <h1 className="text-purple-700 font-bold">This is contact page</h1>
+        </div>
+    )
+}
